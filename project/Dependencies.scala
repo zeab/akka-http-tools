c2175dccc538db4ff98fbbe02c753725d02b8547
kevin downs <pyrosrppltoo@gmail.com>
2018-05-18 16:06:14 -0700

    h

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,7 +6,7 @@ object Dependencies {
 
   //List of Versions
   val V = new {
-    val seeds                     = "4.0.0-RC2"
+    val seeds                     = "4.0.0"
     val akkaHttp                  = "10.1.0-RC1"
     val akka                      = "2.5.9"
     val akkaTestKit               = "2.5.12"
