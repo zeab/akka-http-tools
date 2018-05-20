eeaf9e41da7d15334b6f1e6a460d85fbfcd2cbec
kevin downs <pyrosrppltoo@gmail.com>
2018-05-20 10:24:03 -0700

    up

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,30 +6,31 @@ object Dependencies {
 
   //List of Versions
   val V = new {
-    val seeds                     = "4.0.7"
-    val akkaHttp                  = "10.1.1"
-    val akka                      = "2.5.12"
-    val scalaTest                 = "3.0.5"
+    val akkaHttp                    = "10.1.1"
+    val akka                        = "2.5.12"
+    val scalaTest                   = "3.0.5"
+    val seeds                       = "4.0.7"
   }
 
-  //Units of Work
-  val seeds                       = "zeab" %% "seeds" % V.seeds
-
-  //Akka
-  val akkaStream                  = "com.typesafe.akka" %% "akka-stream" % V.akka
-  val akkaHttp                    = "com.typesafe.akka" %% "akka-http" % V.akkaHttp
-
-  //Test
-  val akkaTestKit                 = "com.typesafe.akka" %% "akka-testkit" % V.akka % Test
-  val scalaTest                   = "org.scalatest" %% "scalatest" % V.scalaTest % "test"
+  //List of Dependencies
+  val D = new {
+    //Seed
+    val seeds                       = "zeab" %% "seeds" % V.seeds
+    //Akka
+    val akkaStream                  = "com.typesafe.akka" %% "akka-stream" % V.akka
+    val akkaHttp                    = "com.typesafe.akka" %% "akka-http" % V.akkaHttp
+    //Test
+    val akkaTestKit                 = "com.typesafe.akka" %% "akka-testkit" % V.akka % Test
+    val scalaTest                   = "org.scalatest" %% "scalatest" % V.scalaTest % "test"
+  }
 
   //Group Common Dependencies
   val commonDependencies: Seq[ModuleID] = Seq(
-    seeds,
-    akkaStream,
-    akkaHttp,
-    akkaTestKit,
-    scalaTest
+    D.seeds,
+    D.akkaStream,
+    D.akkaHttp,
+    D.akkaTestKit,
+    D.scalaTest
   )
 
 }
